99a825606a858ffff7f9ed675f002d756d4b3be1
PEEliza <L22550326@chihuahua2.tecnm.mx>
2025-02-17 14:27:13 -0700

    Modificaciones questions

--- questions.js
+++ questions.js
@@ -1,21 +1,21 @@
-(function(){
-    const titleQuestions = [...document.querySelectorAll('.questions__title')];
-    console.log(titleQuestions)
+// (function(){
+//     const titleQuestions = [...document.querySelectorAll('.questions__title')];
+//     console.log(titleQuestions)
 
-    titleQuestions.forEach(question =>{
-        question.addEventListener('click', ()=>{
-            let height = 0;
-            let answer = question.nextElementSibling;
-            let addPadding = question.parentElement.parentElement;
+//     titleQuestions.forEach(question =>{
+//         question.addEventListener('click', ()=>{
+//             let height = 0;
+//             let answer = question.nextElementSibling;
+//             let addPadding = question.parentElement.parentElement;
 
-            addPadding.classList.toggle('questions__padding--add');
-            question.children[0].classList.toggle('questions__arrow--rotate');
+//             addPadding.classList.toggle('questions__padding--add');
+//             question.children[0].classList.toggle('questions__arrow--rotate');
 
-            if(answer.clientHeight === 0){
-                height = answer.scrollHeight;
-            }
+//             if(answer.clientHeight === 0){
+//                 height = answer.scrollHeight;
+//             }
 
-            answer.style.height = `${height}px`;
-        });
-    });
-})();
+//             answer.style.height = `${height}px`;
+//         });
+//     });
+// })();
